a35de371fe50d3a9fa5a259ff29acc62055c9965
Kamil Mowinski <kamil.mowinski@vulpesoft.pl>
2018-06-17 18:27:17 +0200

    Prepare program to exercise 1

--- main.go
+++ main.go
@@ -2,48 +2,24 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/Mowinski/stx-next-go-workshop/vies"
+	"io"
+	"os"
 )
 
-func readFromStdIn() (data vies.InputData, err error) {
-	fmt.Print("Enter country code: ")
-	_, err = fmt.Scanf("%s", &data.CountryCode)
-	if err != nil {
-		return data, err
-	}
-
-	fmt.Print("Enter VAT: ")
-	_, err = fmt.Scanf("%s", &data.VATNumber)
-	if err != nil {
-		return data, err
-	}
-
+func readFrom(reader io.Reader) (data vies.InputData, err error) {
 	return data, nil
 }
 
-func writeToStdOut(vatDetail *vies.VatDetails) {
-	fmt.Println("Error: ", vatDetail.Error)
-	fmt.Println("ErrorCode: ", vatDetail.ErrorCode)
-	fmt.Println("XMLName: ", vatDetail.XMLName)
-	fmt.Println("CountryCode: ", vatDetail.CountryCode)
-	fmt.Println("VatNumber: ", vatDetail.VatNumber)
-	fmt.Println("RequestDate: ", vatDetail.RequestDate)
-	fmt.Println("Valid: ", vatDetail.Valid)
-	fmt.Println("Name: ", vatDetail.Name)
-	fmt.Println("Address: ", vatDetail.Address)
+func writeToStdOut(writer io.Writer, vatDetail *vies.VatDetails) {
+
 }
 
 func main() {
-	inputData, err := readFromStdIn()
+	inputData, err := readFrom(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 
-	vatDetail, err := vies.Check(inputData)
-	if err != nil {
-		panic(err)
-	}
-
-	writeToStdOut(vatDetail)
+	fmt.Println(inputData)
 }
