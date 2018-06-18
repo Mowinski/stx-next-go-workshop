6f019d36b763680133538157b3b85213f8a0068b
Kamil Mowinski <kamil.mowinski@vulpesoft.pl>
2018-06-18 11:27:28 +0200

    Add task-2

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 )
 
 func readFrom(reader io.Reader) (data vies.InputData, err error) {
+	fmt.Print("Enter country code: ")
+	_, err = fmt.Fscanf(reader, "%s", &data.CountryCode)
+	if err != nil {
+		return data, err
+	}
+
+	fmt.Print("Enter VAT: ")
+	_, err = fmt.Fscanf(reader, "%s", &data.VATNumber)
+	if err != nil {
+		return data, err
+	}
+
 	return data, nil
 }
 
--- main_test.go
+++ main_test.go
@@ -4,7 +4,10 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/Mowinski/stx-next-go-workshop/vies"
 )
 
 func prepareReader(countryCode, number string) (*os.File, error) {
@@ -26,6 +29,12 @@ func prepareReader(countryCode, number string) (*os.File, error) {
 	return reader, nil
 }
 
+func assertEqual(t *testing.T, code, result, expected string) {
+	if result != expected {
+		t.Errorf("Wrong %s, expected '%s', got '%s'", code, expected, result)
+	}
+}
+
 func TestReadingFromFunction(t *testing.T) {
 	reader, err := prepareReader("TEST", "1234567890")
 	defer reader.Close()
@@ -40,11 +49,38 @@ func TestReadingFromFunction(t *testing.T) {
 		t.Errorf("Error expect %s", err)
 	}
 
-	if result.CountryCode != "TEST" {
-		t.Errorf("Wrong CountryCode, expected 'TEST', got '%s'", result.CountryCode)
+	assertEqual(t, "CountryCode", result.CountryCode, "TEST")
+	assertEqual(t, "VATNumber", result.VATNumber, "1234567890")
+}
+
+func TestWritingToFunction(t *testing.T) {
+	vatDetail := vies.VatDetails{
+		CountryCode: "TEST",
+		VatNumber:   "1234567890",
+		Address:     "Test Address",
+		Error:       "",
+		ErrorCode:   "",
+		Name:        "Test name",
+		Valid:       true,
+	}
+	writer, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	if result.VATNumber != "1234567890" {
-		t.Errorf("Wrong VATNUmber, expected '1234567890', got '%s'", result.VATNumber)
-	}
+	writeTo(writer, &vatDetail)
+	writer.Seek(0, os.SEEK_SET)
+
+	bytesResult, _ := ioutil.ReadAll(writer)
+	result := strings.Split(string(bytesResult), "\n")
+
+	assertEqual(t, "Error", result[0], "Error: ")
+	assertEqual(t, "ErrorCode", result[1], "ErrorCode: ")
+	assertEqual(t, "XMLName", result[2], "XMLName: { }")
+	assertEqual(t, "CountryCode", result[3], "CountryCode: TEST")
+	assertEqual(t, "VatNumber", result[4], "VatNumber: 1234567890")
+	assertEqual(t, "RequestDate", result[5], "RequestDate: ")
+	assertEqual(t, "Valid", result[6], "Valid: true")
+	assertEqual(t, "Name", result[7], "Name: Test name")
+	assertEqual(t, "Address", result[8], "Address: Test Address")
 }
